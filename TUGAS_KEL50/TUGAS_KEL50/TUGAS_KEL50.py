5cb6d74014de2d18b3fdd8ea6d8e23f9e92d6925
Abinaya441 <kiko4801@gmail.com>
2020-04-20 19:53:11 +0700

    Kelompok 50

---
+++ TUGAS_KEL50/TUGAS_KEL50/TUGAS_KEL50.py
@@ -0,0 +1,4 @@
+print ("Muhammad Abinaya Isaqofi\t21120119130039")
+print ("Muhammad Dzikrullah Farhan\t21120119140134")
+print ("Kelompok 50\nShift 2\nTUGAS\n\n")
+print ("\t\t\tCOVID-19 RAPID TEST\n")
